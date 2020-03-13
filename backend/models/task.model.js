d014a2920988db7f6abf41d4425f630863cc883b
Dariusz Dudek <darekdede1995@gmail.com>
2020-03-13 18:22:10 +0100

    added task model/route

---
+++ backend/models/task.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const TaskSchema = new Schema({
+    userid: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    finished: {
+        type: Boolean,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = Task;
---
+++ backend/routes/tasks.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+let Task = require('../models/task.model');
+
+router.route('/:id').get((req, res) => {
+    Task.find({
+            userid: req.params.id
+        })
+        .then(groups => res.json(groups))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+
+    const description = req.body.description;
+    const userid = req.body.userid;
+
+    const newTask = new Group({
+        userid: userid,
+        description: description,
+        finished: false
+    });
+
+    newTask.save()
+        .then((group) => res.json(group))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/:id').delete((req, res) => {
+    Task.findByIdAndDelete(req.params.id)
+        .then(() => res.json(req.params.id))
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/update/:id').post((req, res) => {
+
+    Task.findById(req.params.id)
+        .then(task => {
+            task.userid = req.body.userid;
+            task.description = req.body.description;
+            task.finished = req.body.finished;
+
+            task.save()
+                .then((task) => res.json(task))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;
