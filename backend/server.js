f916f87b2ccbc41ff980162e3ccd7ce681f15a7a
Dariusz Dudek <darekdede1995@gmail.com>
2020-03-13 17:43:00 +0100

    init commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//adres bazy
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection established succesfully')
+})
+
+const userRoutes = require('./routes/users');
+const tasksRoutes = require('./routes/tasks');
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', tasksRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/../frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
