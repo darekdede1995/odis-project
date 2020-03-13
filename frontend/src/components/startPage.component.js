5ff2c66e8aefab4dbb4e659d4acf8b309dc71485
Dariusz Dudek <darekdede1995@gmail.com>
2020-03-13 18:55:26 +0100

    added frontend scaffold

---
+++ frontend/src/components/startPage.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/index.css";
+
+function StartPage() {
+  return (
+    <div className="App">
+        <button>LOGIN</button>
+        <button>REGISTER</button>
+    </div>
+  );
+}
+
+export default StartPage;
