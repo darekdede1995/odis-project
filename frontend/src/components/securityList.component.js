679e5d01e845ba646488c42fc66e384665b2897e
Dariusz Dudek <darekdede1995@gmail.com>
2020-03-24 17:55:42 +0100

    added security info frame

--- frontend/src/components/securityList.component.js
+++ frontend/src/components/securityList.component.js
@@ -3,17 +3,60 @@ import { useState } from 'react';
 import '../styles/index.css';
 
 function SecurityList() {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <div className="security-list__container">
       <ul>
-        <li>SQL Injections</li>
-        <li>Cross Site Scripting (XSS)</li>
-        <li>Broken Authentication & Session Management - IdentityManager</li>
-        <li>Insecure Direct Object References - DOM (Document Object Model)</li>
-        <li>Cross-Site Request Forgery (CSRF)</li>
+        <li
+          onClick={() => {
+            toggleInfo(1);
+          }}
+        >
+          SQL Injections
+        </li>
+        <li
+          onClick={() => {
+            toggleInfo(2);
+          }}
+        >
+          Cross Site Scripting (XSS)
+        </li>
+        <li
+          onClick={() => {
+            toggleInfo(3);
+          }}
+        >
+          Broken Authentication & Session Management - IdentityManager
+        </li>
+        <li
+          onClick={() => {
+            toggleInfo(4);
+          }}
+        >
+          Insecure Direct Object References - DOM (Document Object Model)
+        </li>
+        <li
+          onClick={() => {
+            toggleInfo(5);
+          }}
+        >
+          Cross-Site Request Forgery (CSRF)
+        </li>
       </ul>
+      {showInfo ? (
+        <div className="security--info">
+          tutaj info o kazdym zagrozeniu eldo
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
+
+  function toggleInfo(index) {
+    setShowInfo(prev => !prev);
+  }
 }
 
 export default SecurityList;
