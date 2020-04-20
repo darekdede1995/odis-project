8efda9ae816dd0f6b08b55024e2f0ddb29fbe9ed
Dariusz Dudek <darekdede1995@gmail.com>
2020-04-20 15:17:34 +0200

    added tests structure

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,39 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login element', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const loginElement = getByText(/login/i);
+  expect(loginElement).toBeInTheDocument();
+});
+
+test('renders register elements', () => {
+  const { getByText } = render(<App />);
+
+  const registerElement = getByText(/register/i);
+  expect(registerElement).toBeInTheDocument();
+});
+
+test('renders tasks elements', () => {
+  const { getByText } = render(<App />);
+
+  const tasksElement = getByText(/tasks/i);
+  expect(tasksElement).toBeInTheDocument();
+});
+
+test('renders comments elements', () => {
+  const { getByText } = render(<App />);
+
+  const commentsElement = getByText(/comments/i);
+
+  expect(commentsElement).toBeInTheDocument();
+});
+
+test('renders secure elements', () => {
+  const { getByText } = render(<App />);
+
+  const secureElement = getByText(/secure/i);
+
+  expect(secureElement).toBeInTheDocument();
 });
